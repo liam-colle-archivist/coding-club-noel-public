5011224600b90e169722063cd12674956644eacf
Liam Colle <liam.colle@epitech.eu>
2024-12-12 15:39:14 +0100

    fix: Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,16 +1,15 @@
-...
+fichier = open("./...", "r")
 decalage = ...
 
 texte = fichier....()
-fichier....()
 resultat = ""
 for i in ...(len(...)):
     charactere = texte[i]
     if ....isalpha():
-        if charactere....():
+        if ....isupper():
             resultat += chr((...(charactere) + ... - 65) % ... + 65)
         ...:
-            resultat += ...((...(...) + decalage - ...) % 26 + 97)
+            resultat += ...((ord(...) + decalage - ...) % 26 + 97)
     ...:
         resultat += ...
 
